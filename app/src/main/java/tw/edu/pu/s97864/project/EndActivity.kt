7093b16db7c13a7c29049ecc68f967bea6043ce4
s1081703 <s1081703@gm.pu.edu.tw>
2021-06-22 10:49:23 +0800

    1

---
+++ app/src/main/java/tw/edu/pu/s97864/project/EndActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.s97864.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EndActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end)
+    }
+}
